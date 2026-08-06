867a8f8990f0e368f7cf0ef9920ecc153570e96a
Anilton Vigânigo Júnior <avjrawscloudstudies@gmail.com>
2026-08-05 23:05:43 -0300

    feat: adiciona localização da pasta de entrada

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+from pathlib import Path  
+
 def main() -> None:
     """Ponto de entrada da automação."""
     print("Automação iniciada com sucesso!")
-
+    
+    #Localizar pasta de entrada de arquivos
+    pasta_entrada = Path(__file__).parent / "data" / "input"
+    print(f"Pasta de entrada: {pasta_entrada.resolve()}")
+    
 if __name__ == "__main__":
     main()
